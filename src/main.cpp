567d43307818f7f065355cd3f80611a5fa4639bd
tctlrd <accts@xsxs.in>
2024-12-24 06:19:06 -0600

    removed Utils from Emü and added his library for future mifare desfire support

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 /*
 MIT License
 
+Augmented by tctlrd 2025
 Copyright (c) 2024 pvtex
 Copyright (c) 2018 esp-rfid Community
 Copyright (c) 2017 Ömer Şiar Baysal
@@ -23,7 +24,8 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
  */
-#define VERSION "1.1.9"
+
+#define VERSION "0.2.0"
 
 #ifdef ETHERNET
 bool eth_connected = false;
@@ -44,6 +46,8 @@ bool eth_connected = false;
 #include <Ticker.h>
 #include <AsyncMqttClient.h>
 #include <Bounce2.h>
+#include <Desfire.h>
+#include <PN532.h>
 //#include <esp_task_wdt.h>
 #include <Update.h>
 #include "magicnumbers.h"
@@ -74,6 +78,46 @@ WiegandNG wg;
 	bool activateRelay[MAX_NUM_RELAYS] = {false};
 	bool deactivateRelay[MAX_NUM_RELAYS] = {false};
 #endif
+
+Desfire desfire;
+
+// The PICC master key.
+// This 3K3DES or AES key is the "god key".
+// It allows to format the card and erase ALL it's content (except the PICC master key itself).
+// This key will be stored on your Desfire card when you execute the command "ADD {Username}" in the terminal.
+// To restore the master key to the factory default DES key use the command "RESTORE" in the terminal.
+// If you set the compiler switch USE_AES = true, only the first 16 bytes of this key will be used.
+// IMPORTANT: Before changing this key, please execute the RESTORE command on all personalized cards!
+// IMPORTANT: When you compile for DES, the least significant bit (bit 0) of all bytes in this key 
+//            will be modified, because it stores the key version.
+const byte SECRET_PICC_MASTER_KEY[24] = { 0xAA, 0x08, 0x57, 0x92, 0x1C, 0x76, 0xFF, 0x65, 0xE7, 0xD2, 0x78, 0x44, 0xF8, 0x0F, 0x8D, 0x1B, 0xE7, 0xC2, 0xF0, 0x89, 0x04, 0xC0, 0xC3, 0xE3 };
+
+// This 3K3DES key is used to derive a 16 byte application master key from the UID of the card and the user name.
+// The purpose is that each card will have it's unique application master key that can be calculated from known values.
+const byte SECRET_APPLICATION_KEY[24] = { 0x81, 0xDF, 0x6A, 0xD9, 0x89, 0xE9, 0xA2, 0xD1, 0xC5, 0xB3, 0xE3, 0x9D, 0xE9, 0x60, 0x43, 0xE3, 0x5B, 0x60, 0x85, 0x8B, 0x99, 0xD8, 0xD3, 0x5B };
+
+// This 3K3DES key is used to derive the 16 byte store value from the UID of the card and the user name.
+// This value is stored in a standard data file on the card.
+// The purpose is that each card will have it's unique store value that can be calculated from known values.
+const byte SECRET_STORE_VALUE_KEY[24] = { 0x1E, 0x5D, 0x78, 0x57, 0x68, 0xFC, 0xEE, 0xC9, 0x40, 0xEC, 0x30, 0xDE, 0xEC, 0xA9, 0x8B, 0x3C, 0x7F, 0x8A, 0xC9, 0xC3, 0xAA, 0xD7, 0x4F, 0x17 };
+
+// -----------------------------------------------------------------------------------------------------------
+
+// The ID of the application to be created
+// This value must be between 0x000001 and 0xFFFFFF (NOT zero!)
+const uint32_t CARD_APPLICATION_ID = 0xAA401F;
+
+// The ID of the file to be created in the above application
+// This value must be between 0 and 31
+const byte CARD_FILE_ID = 0;
+
+// This 8 bit version number is uploaded to the card together with the key itself.
+// This version is irrelevant for encryption. 
+// It is just a version number for the key that you can obtain with Desfire::GetKeyVersion().
+// The key version can always be obtained without authentication.
+// You can theoretically have multiple master keys and by obtaining the version you know which one to use for authentication.
+// This value must be between 1 and 255 (NOT zero!)
+const byte CARD_KEY_VERSION = 0x10;
 
 // these are from vendors
 #include "webh/glyphicons-halflings-regular.woff.gz.h"
@@ -158,7 +202,7 @@ void ICACHE_FLASH_ATTR setup()
 	Serial.begin(115200);
 	Serial.println();
 
-	Serial.print(F("[ INFO ] ESP RFID v"));
+	Serial.print(F("[ INFO ] ESP32-ACTL v"));
 	Serial.print(VERSION);
 #ifdef ETHERNET
 	Serial.print(" eth");
@@ -250,6 +294,12 @@ void ICACHE_FLASH_ATTR setup()
 #ifdef DEBUG
 	Serial.println(F("[ INFO ] System setup completed, running"));
 #endif
+	desfire.setPiccMasterKey(SECRET_PICC_MASTER_KEY);
+	desfire.setApplicationKey(SECRET_APPLICATION_KEY);
+	desfire.setStoreValueKey(SECRET_STORE_VALUE_KEY);
+	desfire.setCardApplicationId(CARD_APPLICATION_ID);
+	desfire.setCardFileId(CARD_FILE_ID);
+	desfire.setCardKeyVersion(CARD_KEY_VERSION);
 }
 
 void ICACHE_RAM_ATTR loop()
