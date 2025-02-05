b754179d85ee9627e0f317e138ed54cf56ad0584
tctlrd <accts@xsxs.in>
2025-02-05 12:41:52 -0600

    gulpfile functioning well

--- tools/webfilesbuilder/gulpfile.js
+++ tools/webfilesbuilder/gulpfile.js
@@ -2,111 +2,115 @@ const gulp = require('gulp');
 const fs = require('fs-extra');
 const zlib = require('zlib');
 const concat = require('gulp-concat');
-const flatmap = require('gulp-flatmap');
 const path = require('path');
 const htmlmin = require('gulp-htmlmin');
 const uglify = require('gulp-uglify');
-const pump = require('pump');
-
-const srcDir = '../../src/websrc/';
-const webhDir = '../../src/webh/';
-const uiDir = `${srcDir}ui/`;
-const thDir = `${srcDir}3rdparty/`;
-const prDir = `${srcDir}process/`;
+const websrc = '../../src/websrc/';
+const webh = `../../src/webh/`;
+const tpDir = `${websrc}3rdparty/`;
+const prDir = `${websrc}process/`;
+const uiDir = `${websrc}ui/`;
 const mnDir = `${prDir}mini/`;
 const fnDir = `${prDir}final/`;
 const gzDir = `${prDir}gzip/`;
-
+const directories = [websrc,webh,tpDir,prDir,uiDir,mnDir,fnDir,gzDir];
 const mn = 'required';
 
-// minify javascript files
-function minifyScripts(srcFolder, destFolder) {
+// Recreate output directories (destructive)
+const ensureDirs = (cb) => {
+    console.log('RECREAT OUTPUT DIRECTORIES (destructive of: webh & process)');
+    fs.removeSync(webh);
+    fs.removeSync(prDir);
+    directories.forEach(dir => {fs.ensureDirSync(dir);});
+    cb();
+};
+
+// Minify JavaScript files
+function minify(srcFolder, destFolder) {
     return gulp.src(`${srcFolder}*.js`)
         .pipe(uglify())
         .pipe(gulp.dest(destFolder));
 }
 
-// concate style and script files into single [.js|.css] files
+// Concat files into a single file.
 function merge(srcFolder, fileType, destDir, outputFile) {
     return gulp.src(`${srcFolder}*.${fileType}`)
-        .pipe(concat({
-            path: outputFile,
-            stat: {
-                mode: 0o666
-            }
-        }))
+        .pipe(concat({path: outputFile, stat: { mode: 0o666 }}))
         .pipe(gulp.dest(destDir));
 }
 
-// gzip all files before converting them to to byte arrays
-function gzip(src, dest) {
-    return function (cb) {
-        fs.readdirSync(src).forEach(file => {
-            const filePath = path.join(src, file);
-            const gzFilePath = path.join(dest, file + '.gz');
-            const input = fs.readFileSync(filePath);
-            const output = zlib.gzipSync(input);
-            fs.writeFileSync(gzFilePath, output);
-        });
-        cb();
-    };
+// Gzip files
+function gzip(srcFile, destFile, cb) {
+    const input = fs.readFileSync(srcFile);
+    const output = zlib.gzipSync(input);
+    fs.writeFileSync(destFile, output);
+    cb();
 }
 
-// create byte arrays of gzipped files outputing .gz.h header files into webh directory
-// these are the final processed version of the files, and are included in the firmware
+// Create byte arrays of gzipped files
 function byteArray(source, destination, name, cb) {
     const arrayName = name.replace(/\.|-/g, "_");
-    try {
-        const data = fs.readFileSync(source);
-        const wstream = fs.createWriteStream(destination);
-        wstream.on('error', console.log);
-
-        wstream.write(`#define ${arrayName}_len ${data.length}\n`);
-        wstream.write(`const uint8_t ${arrayName}[] PROGMEM = {`);
-
-        for (let i = 0; i < data.length; i++) {
-            if (i % 1000 === 0) wstream.write('\n');
-            wstream.write('0x' + data[i].toString(16).padStart(2, '0') + (i < data.length - 1 ? ',' : ''));
-        }
-
-        wstream.write('\n};');
-        wstream.end(cb);
-    } catch (error) {
-        console.error('Error processing gzipped file:', error);
-        cb(error);
+    const data = fs.readFileSync(source);
+    const wstream = fs.createWriteStream(destination);
+    wstream.write(`#define ${arrayName}_len ${data.length}\n`);
+    wstream.write(`const uint8_t ${arrayName}[] PROGMEM = {`);
+    for (let i = 0; i < data.length; i++) {
+        if (i % 1000 === 0) wstream.write('\n');
+        wstream.write('0x' + data[i].toString(16).padStart(2, '0') + (i < data.length - 1 ? ',' : ''));
     }
+    wstream.write('\n};');
+    wstream.end(cb);
 }
 
-function scripts (){
-    let type = 'js';
-    gulp.series(
-        minifyScripts(`${thDir}${type}/`, `${mnDir}${type}/`)/*,
-        merge(`${mnDir}${type}/`, `${type}`, `${mnDir}/${type}/`, `${mn}.${type}`),
-        gzip(`${mnDir}${type}/`, `${gzDir}${type}/`),
-        byteArray(`${gzDir}${type}/${mn}.${type}.gz`, `${webhDir}/${mn}.${type}.gz.h`, `${mn}_${type}_gz`)*/
-    );
+// Task: Process assets (scripts, UI scripts, HTML, styles, fonts)
+const process = (cb) => {
+    console.log('PROCESS UI & 3RDPARTY FILES TO FINAL: minify, merge, copy');
+    const minifyThirdPartyScripts = () => minify(`${tpDir}js/`, `${mnDir}`);
+    const mergeThirdPartyScripts = () => merge(`${mnDir}`, 'js', `${fnDir}`, `${mn}.js`);
+    const minifyUIScripts = () => minify(`${uiDir}`, `${fnDir}`);
+    const minifyHTMLFiles = () => gulp.src(`${uiDir}*.htm*`)
+        .pipe(htmlmin({ collapseWhitespace: true, minifyJS: true }).on('error', console.error))
+        .pipe(gulp.dest(`${fnDir}`));
+    const mergeCSSFiles = () => merge(`${tpDir}css/`, 'css', `${fnDir}`, `${mn}.css`);
+    const copyFonts = () => gulp.src(`${tpDir}fonts/*.*`).pipe(gulp.dest(`${fnDir}`));
+
+    gulp.parallel(
+        gulp.series(minifyThirdPartyScripts, mergeThirdPartyScripts),
+        minifyUIScripts,
+        minifyHTMLFiles,
+        mergeCSSFiles,
+        copyFonts
+    )(cb);
 };
 
-/*
-const styles = gulp.series(
-    concat(),
-    gzip(),
-    byteArray(`${gzippedDir}css/required.css.gz`, `${webhDir}required.css.gz.h`, 'required_css_gz')
-);
+// Task: Gzip files
+function gzipAll(cb) {
+    console.log('GZIP FINAL FILES');
+    const files = fs.readdirSync(fnDir);
+    const tasks = files.map((file) => {
+        const srcFile = path.join(fnDir, file);
+        const destFile = path.join(gzDir, file + '.gz');
+        const taskName = (done) => gzip(srcFile, destFile, done);
+        Object.defineProperty(taskName, 'name', { value: `${file}.gz` });
+        return taskName;
+    });
+    gulp.parallel(...tasks)(cb);
+}
 
-const fonts = gulp.series(
-    gzip();
-    byteArray();
-);
+// Task: Create byte arrays from gzipped files
+function byteArrayAll(cb) {
+    console.log('BYTE ARRAY GZIP FILES');
+    const files = fs.readdirSync(gzDir);
+    const tasks = files.map(file => {
+        const srcFile = path.join(gzDir, file);
+        const destFile = `${webh}${file}.h`;
+        const taskName = (done) => byteArray(srcFile, destFile, file, done);
+        Object.defineProperty(taskName, 'name', { value: `${file}.h` });
+        return taskName;
+    });
+    gulp.parallel(...tasks)(cb);
+}
 
-const html = gulp.series(
-    function htmlsPrep() {
-        return gulp.src(`${srcDir}/esp-ui/*.htm*`)
-            .pipe(htmlmin({ collapseWhitespace: true, minifyJS: true }))
-            .pipe(gulp.dest(gzippedDir));
-    },
-    function htmls() {
-);
-*/
-
-exports.default = scripts() /*gulp.parallel(scripts, styles, fonts, html)*/;
+// Main runner function
+function runner(cb) { gulp.series(ensureDirs, process, gzipAll, byteArrayAll)(cb); }
+exports.default = runner;
