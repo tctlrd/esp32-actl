273f5420d8e79fe22a2fd14a5856db482473a62a
tctlrd <accts@xsxs.in>
2025-02-06 03:09:09 -0600

    corrected javascript concat order through name prefix

--- tools/webfilesbuilder/gulpfile.js
+++ tools/webfilesbuilder/gulpfile.js
@@ -13,7 +13,16 @@ const uiDir = `${websrc}ui/`;
 const mnDir = `${prDir}mini/`;
 const fnDir = `${prDir}final/`;
 const gzDir = `${prDir}gzip/`;
-const directories = [websrc,webh,tpDir,prDir,uiDir,mnDir,fnDir,gzDir];
+const directories = [
+    websrc,
+    webh,
+    tpDir,
+    prDir,
+    uiDir,
+    mnDir,
+    fnDir,
+    gzDir
+];
 const mn = 'required';
 
 // Recreate output directories (destructive)
@@ -21,7 +30,9 @@ const ensureDirs = (cb) => {
     console.log('RECREAT OUTPUT DIRECTORIES (destructive of: webh & process)');
     fs.removeSync(webh);
     fs.removeSync(prDir);
-    directories.forEach(dir => {fs.ensureDirSync(dir);});
+    directories.forEach(dir => {
+        fs.ensureDirSync(dir);
+    });
     cb();
 };
 
@@ -32,7 +43,12 @@ function minify(srcFolder, destFolder) {
 
 // Concat files into a single file.
 function merge(srcFolder, fileType, destDir, outputFile) {
-    return gulp.src(`${srcFolder}*.${fileType}`).pipe(concat({path: outputFile, stat: { mode: 0o666 }})).pipe(gulp.dest(destDir));
+    return gulp.src(`${srcFolder}*.${fileType}`).pipe(concat({
+        path: outputFile,
+        stat: {
+            mode: 0o666
+        }
+    })).pipe(gulp.dest(destDir));
 }
 
 // Gzip files
@@ -51,7 +67,8 @@ function byteArray(source, destination, name, cb) {
     wstream.write(`#define ${arrayName}_len ${data.length}\n`);
     wstream.write(`const uint8_t ${arrayName}[] PROGMEM = {`);
     for (let i = 0; i < data.length; i++) {
-        if (i % 1000 === 0) wstream.write('\n');
+        if (i % 1000 === 0) 
+            wstream.write('\n');
         wstream.write('0x' + data[i].toString(16).padStart(2, '0') + (i < data.length - 1 ? ',' : ''));
     }
     wstream.write('\n};');
@@ -61,13 +78,14 @@ function byteArray(source, destination, name, cb) {
 // Task: Process assets (scripts, UI scripts, HTML, styles, fonts)
 const process = (cb) => {
     console.log('PROCESS UI & 3RDPARTY FILES TO FINAL: minify, merge, copy');
-    const minifyJS = () => minify(`${tpDir}js/`, `${mnDir}`);
-    const mergeJS = () => merge(`${mnDir}`, 'js', `${fnDir}`, `${mn}.js`);
+    const mergeJS = () => merge(`${tpDir}js/`, 'js', `${fnDir}`, `${mn}.js`);
     const mergeCSS = () => merge(`${tpDir}css/`, 'css', `${fnDir}`, `${mn}.css`);
     const minifyUIjs = () => minify(`${uiDir}`, `${fnDir}`);
-    const minifyUIhtml = () => gulp.src(`${uiDir}*.htm*`).pipe(htmlmin({ collapseWhitespace: true, minifyJS: true }).on('error', console.error)).pipe(gulp.dest(`${fnDir}`));
-    const copyFonts = (cb) => {fs.copy(`${tpDir}fonts/`, `${fnDir}`, cb);};
-    gulp.parallel(gulp.series(minifyJS, mergeJS), minifyUIjs, minifyUIhtml, mergeCSS, copyFonts)(cb);
+    const minifyUIhtml = () => gulp.src(`${uiDir}*.htm*`).pipe(htmlmin({collapseWhitespace: true, minifyJS: true}).on('error', console.error)).pipe(gulp.dest(`${fnDir}`));
+    const copyFonts = (cb) => {
+        fs.copy(`${tpDir}fonts/`, `${fnDir}`, cb);
+    };
+    gulp.parallel(mergeJS, minifyUIjs, minifyUIhtml, mergeCSS, copyFonts)(cb);
 };
 
 // Task: Gzip files
@@ -78,10 +96,10 @@ function gzipAll(cb) {
         const srcFile = path.join(fnDir, file);
         const destFile = path.join(gzDir, file + '.gz');
         const taskName = (done) => gzip(srcFile, destFile, done);
-        Object.defineProperty(taskName, 'name', { value: `${file}.gz` });
+        Object.defineProperty(taskName, 'name', {value: `${file}.gz`});
         return taskName;
     });
-    gulp.parallel(...tasks)(cb);
+    gulp.parallel(... tasks)(cb);
 }
 
 // Task: Create byte arrays from gzipped files
@@ -92,12 +110,14 @@ function byteArrayAll(cb) {
         const srcFile = path.join(gzDir, file);
         const destFile = `${webh}${file}.h`;
         const taskName = (done) => byteArray(srcFile, destFile, file, done);
-        Object.defineProperty(taskName, 'name', { value: `${file}.h` });
+        Object.defineProperty(taskName, 'name', {value: `${file}.h`});
         return taskName;
     });
-    gulp.parallel(...tasks)(cb);
+    gulp.parallel(... tasks)(cb);
 }
 
 // Main runner function
-function runner(cb) { gulp.series(ensureDirs, process, gzipAll, byteArrayAll)(cb); }
+function runner(cb) {
+    gulp.series(ensureDirs, process, gzipAll, byteArrayAll)(cb);
+}
 exports.default = runner;
